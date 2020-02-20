fc96ac07da7c4f834a6539acfd5f60eb622c0821
sathyadevops <sathyadevops1@gmail.com>
2020-02-20 15:15:34 +0000

    Java code

---
+++ demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+  p.s.void main(...)
+  {
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+    s.o.print(" Good day");
+  }
+}
